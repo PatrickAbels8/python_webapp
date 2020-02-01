a8c6f67f40bc7105b6698e51d5149dbd465f4362
patri <pabels@students.uni-mainz.de>
2020-02-01 19:33:09 +0100

    ready for jsons

--- api.py
+++ api.py
@@ -1,32 +1,46 @@
-from flask import Flask, render_template, escape, url_for, redirect
+from flask import Flask, request, render_template, escape, url_for, redirect
 import os
+
+# MAIN FUNCTION
+def getData(json):
+	return 'Search for %s gives: tba' % json
 
 # create Flask with reference to style sheet
 app = Flask(__name__, static_url_path='/static')
 DEFAULT_PORT = 2000
 
-# connect url / with say_hello method
+# infos about HTTP: https://www.tutorialspoint.com/flask/flask_http_methods.htm
 @app.route('/')
-def say_hello():
-	return 'Hello, World!'
+@app.route('/home', methods=['POST', 'GET'])
+def home():
+	if request.method == 'GET':
+		return render_template('home.html')
+	elif request.method == 'POST':
+		return getData(request.form['json'])
 
-# use variables in url
-@app.route('/user/<username>')
-def show_username(username):
-	return 'Welcome, User %s!' % escape(username)
-
-# start html template
 @app.route('/doc')
-def open_documentation():
+@app.route('/docu')
+@app.route('/documentation')
+def documentation():
  	return render_template('doc.html')
 
-# pages that aren't finished yet
 @app.route('/images')
-@app.route('/about')
-def load_images():
-	return redirect(url_for('inprogress'))
+@app.route('/imgs')
+def images():
+	return render_template('images.html')
 
+@app.route('/about')
+def about():
+	return render_template('about.html')
+
+@app.route('/references')
+@app.route('/ref')
+@app.route('/refs')
+def referneces():
+	return render_template('references.html')
+	
 # progress page for all the redirects
+# use with: return redirect(url_for('inprogress'))
 @app.route('/inprogress')
 def inprogress():
 	return 'not finished yet... :('
